a03668196bcf6036f9bc6b69e2ebc5338f71fab9
Tenzin Dorjee <tdorjee@middlebury.edu>
2018-01-13 18:27:38 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,28 @@
-# Define the variable board below.
+board = [ " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# left top
+board[0]
+
+# center top
+board[1]
+
+# right top
+board[2]
+
+# left center
+board[3]
+
+# center
+board[4]
+
+# right center
+board[5]
+
+# left bottom
+board[6]
+
+# center bottom
+board[7]
+
+# right bottom
+board[8]
